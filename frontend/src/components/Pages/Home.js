ed6c5de6f83a195646a5380113a00ddba2c9607d
Anthony Quinn <anthonyquinn.developer@gmail.com>
2024-05-25 21:19:17 +0100

    adding in conditional check if user is logged in to show other buttons, otherwise, show sign in button

--- frontend/src/components/Pages/Home.js
+++ frontend/src/components/Pages/Home.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import SignInButton from '../Button/SignInButton';
-import './Home.css'; 
+import './Home.css';
 
 const Home = () => {
   const navigate = useNavigate();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    // Check if the user is logged in by checking local storage
+    const userData = localStorage.getItem('userData');
+    if (userData) {
+      setIsLoggedIn(true);
+    }
+  }, []);
 
   const handleSignIn = () => {
     window.location.href = 'http://localhost:8080/google_login';
@@ -17,13 +26,18 @@ const Home = () => {
   return (
     <div className="container">
       <h1 className="title">Welcome to Lock and Learn</h1>
-      <p className="message">Please sign in to continue:</p>
-      <SignInButton onSignIn={handleSignIn} />
-      <div className="buttonContainer">
-        <button onClick={() => navigateTo('/encrypt')}>Encrypt & Learn</button>
-        <button onClick={() => navigateTo('/decrypt')}>Decrypt & Learn</button>
-        <button onClick={() => navigateTo('/user')}>User Page</button>
-      </div>
+      {!isLoggedIn ? (
+        <>
+          <p className="message">Please sign in to continue:</p>
+          <SignInButton onSignIn={handleSignIn} />
+        </>
+      ) : (
+        <div className="buttonContainer">
+          <button onClick={() => navigateTo('/encrypt')}>Encrypt & Learn</button>
+          <button onClick={() => navigateTo('/decrypt')}>Decrypt & Learn</button>
+          <button onClick={() => navigateTo('/user')}>User Page</button>
+        </div>
+      )}
     </div>
   );
 };
