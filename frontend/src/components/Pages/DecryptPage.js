5aa7aa7a2c9b42245f0b04ac094eff5cf82ca30d
Anthony Quinn <anthonyquinn.developer@gmail.com>
2024-05-28 22:15:17 +0100

    comments of what ill be working on in the coming days

--- frontend/src/components/Pages/DecryptPage.js
+++ frontend/src/components/Pages/DecryptPage.js
@@ -12,6 +12,15 @@ const DecryptPage = ({ onDecrypt }) => {
   return (
     <div className="container">
       <h2 className="title">Decrypt & Learn</h2>
+
+      {/* NEED TO ADD LEARNING PLATFORM DIALOGS TO EXPLAIN THE ENCRYPTION
+      
+      1. Check if user has decrypted before (check db against their oauth id)
+      2. Is there an entry? If so, allow them to decrypt + show learning
+      3. Is there not an entry in the db? Direct them to do the quiz first.
+      4. tbd
+      
+      */}
       <form onSubmit={handleSubmit} className="form">
         <input
           type="text"
--- frontend/src/components/Pages/EncryptPage.js
+++ frontend/src/components/Pages/EncryptPage.js
@@ -12,6 +12,16 @@ const EncryptPage = ({ onEncrypt }) => {
   return (
     <div className="container">
       <h2 className="title">Encrypt & Learn</h2>
+
+      {/* NEED TO ADD LEARNING PLATFORM DIALOGS TO EXPLAIN THE ENCRYPTION
+      
+      1. Check if user has encrypted before (check db against their oauth id)
+      2. Is there an entry? If so, allow them to encrypt + show learning
+      3. Is there not an entry in the db? Direct them to do the quiz first.
+      4. tbd
+      
+      */}
+
       <form onSubmit={handleSubmit} className="form">
         <input
           type="text"
